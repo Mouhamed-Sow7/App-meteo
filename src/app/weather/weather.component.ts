33c422601396506db8b9e45eff1d1f331b8f296f
medz-7 <mouhamed.sow7@unchk.edu.sn>
2025-01-15 23:15:29 +0000

    ajout de champs

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -3,15 +3,16 @@ import { WeatherService } from '../weather.service';
 @Component({
   selector: 'app-weather',
   standalone: false,
-  
+
   templateUrl: './weather.component.html',
-  styleUrl: './weather.component.css'
+  styleUrl: './weather.component.css',
 })
 export class WeatherComponent {
   city: string = 'Dakar';
   weatherData: any = null;
   errorMessage: string = '';
-
+  setsun: string = '';
+  risesun: string = '';
   constructor(private weatherService: WeatherService) {}
 
   getWeather(): void {
@@ -19,12 +20,16 @@ export class WeatherComponent {
       next: (data) => {
         this.weatherData = data;
         this.errorMessage = '';
+        let lever = new Date(this.weatherData.sys.sunrise * 1000);
+        this.risesun = lever.toLocaleTimeString();
+        let coucher = new Date(this.weatherData.sys.sunset * 1000);
+        this.setsun = coucher.toLocaleTimeString();
       },
       error: (err) => {
         this.weatherData = null;
         this.errorMessage = 'Ville introuvable ou problème de connexion.';
-      }
+      },
     });
   }
-
+  getweatherDate(): void {}
 }
